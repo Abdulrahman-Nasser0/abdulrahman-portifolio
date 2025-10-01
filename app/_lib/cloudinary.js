6e90b2bfe1aeb4861b729b6889bdb5914f866380
Abdulrahman Nasser <abdulrahman.nasser222@gmail.com>
2025-10-01 19:17:53 +0300

    chore(lint): add blank line between import groups in cloudinary wrapper

--- app/_lib/cloudinary.js
+++ app/_lib/cloudinary.js
@@ -2,8 +2,8 @@
 
 // Wrapper around next-cloudinary that gracefully degrades when env vars are missing
 import React from 'react';
+
 import { CldImage as RealCldImage, CldVideoPlayer as RealCldVideoPlayer } from 'next-cloudinary';
-// import-group-separator
 const hasCloudName = !!process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
 
 function Placeholder({ className, style, alt = 'media placeholder', fill, width, height, type = 'image' }) {
